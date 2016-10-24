f933c6d0298b639bb329d6b3bdfe2d89b2b7ac5d
Michał Łasecki <lasecki.mic@gmail.com>
2016-10-24 13:40:02 +0200

    some ugly js

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,1 +1,32 @@
 $('.parallax').parallax();
+
+// LOADER
+$(document).ready(function() {
+  setTimeout(function(){
+      $('body').addClass('loaded');
+  }, 3000);
+
+// FULL MENU
+// ZROBIĆ FUNKCJE Z TEGO
+  $('#btn').click(function() {
+    $(this).toggleClass('active');
+    $('#menu').toggleClass('open');
+    $('main').toggleClass('hide');
+  });
+  $('.menu-list').on('click', 'li', function(e) {
+    $('#btn').toggleClass('active');
+    $('#menu').toggleClass('open');
+    $('main').toggleClass('hide');
+  });
+
+// PAGE SCROLL
+  $('a.page-scroll').bind('click', function(event) {
+    event.preventDefault();
+    var $anchor;
+    $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top -10
+    }, 1250, 'easeInOutExpo');
+  });
+
+});
