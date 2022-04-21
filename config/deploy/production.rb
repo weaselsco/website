7a6e1f317cab585b270c5baaedadf95096496364
Mike Łasecki <michal.lasecki@firma.interia.pl>
2022-04-21 23:35:32 +0200

    new pictures

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,4 +10,4 @@ set :use_sudo, false
 set :deploy_to, '/home/coderschool/app'
 set :branch, 'master'
 
-server 'ok.lasica.co', user: 'coderschool', roles: %w{web app db}
+server '46.101.154.156', user: 'coderschool', roles: %w{web app db}
